07405d2b94524e42050e503cec46ba3df42d5073
TheMindset <mulezig2014@outlook.fr>
2020-01-08 07:55:29 +0100

    Refactor apiCalls.js

--- src/component/api/apiCalls.js
+++ src/component/api/apiCalls.js
@@ -5,8 +5,19 @@ export const getFilms = () => {
       throw new Error(`Error: ${response.status}, please retry`)
     }
     return response.json()
-  }) 
+  })
   .then(data => data.results)
-  .then(allMovies => allMovies.sort((a, b) => a.episode_id - b.episode_id)
+  .then(allFilms => sortFilmsByEpisode(allFilms))
   .catch(error => console.log(error))
 }
+
+const sortFilmsByEpisode = (allFilms) => {
+  return allFilms.sort((a, b) => a.episode_id - b.episode_id)
+  .map(film => {
+    return {
+      title: film.title,
+      episode: film.episode_id,
+      releaseYear: film.release_date.slice(0, 4)
+    }
+  })
+} 
