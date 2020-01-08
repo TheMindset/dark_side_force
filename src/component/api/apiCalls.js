c3422d316f512569b741747b6900dbb7c15ca567
TheMindset <mulezig2014@outlook.fr>
2020-01-08 08:43:00 +0100

    Add logic for fetch characters by film

--- src/component/api/apiCalls.js
+++ src/component/api/apiCalls.js
@@ -13,11 +13,51 @@ export const getFilms = () => {
 
 const sortFilmsByEpisode = (allFilms) => {
   return allFilms.sort((a, b) => a.episode_id - b.episode_id)
-  .map(film => {
+  .map((film, i) => {
     return {
       title: film.title,
+      id: i + 1,
       episode: film.episode_id,
       releaseYear: film.release_date.slice(0, 4)
     }
   })
 } 
+
+
+export const getCharacters = (id) => {
+  const url = `https://swapi.co/api/films/${id}`
+
+  return fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}, please retry`)
+    }
+    return response.json()
+  })
+  .then(data => data.characters)
+  .then(data => cleanCharacterData(data))
+  .then()
+}
+
+
+const cleanCharacterData = (characterUrls) => {
+  const characterData = characterUrls.map(charUrl => {
+    return fetch(charUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}, please retry`)
+      }
+      return response.json()
+    })
+    .then(data => {
+      return {
+        name: data.name,
+        homeworld: data.homeworld,
+        species: data.species,
+        films: data.films
+      }
+
+    })
+  })
+  return Promise.all(characterData)
+}
