72662e92a085b31ec44bfb25e91a7fb8bc15c6aa
TheMindset <mulezig2014@outlook.fr>
2020-01-09 14:20:53 +0100

    Clean code

--- src/component/api/apiCalls.js
+++ src/component/api/apiCalls.js
@@ -1,58 +1,83 @@
 export const getFilms = () => {
   return fetch('https://swapi.co/api/films')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}, please retry`)
-    }
-    return response.json()
-  })
-  .then(data => data.results)
-  .then(allFilms => sortFilmsByEpisode(allFilms))
-  .catch(error => console.log(error))
+    .then(response => {
+      if(!response.ok) {
+        throw Error('Could not fetch films, please refresh the page and try again')
+      }
+      return response.json()})
+    .then(data => sortFilmsByReleaseDate(data))
 }
 
-const sortFilmsByEpisode = (allFilms) => {
-  return allFilms.sort((a, b) => a.episode_id - b.episode_id)
-  .map((film, i) => {
+const sortFilmsByReleaseDate = (data) => {
+  return data.results.sort((a, b) => {
+    let yearA = parseInt(a.release_date.slice(0, 4));
+    let yearB = parseInt(b.release_date.slice(0, 4));
+    return yearA - yearB
+  }).map((movie,i) => {
     return {
-      title: film.title,
+      title: movie.title,
+      scrollText: movie.opening_crawl,
       id: i + 1,
-      episode: film.episode_id,
-      releaseYear: film.release_date.slice(0, 4)
+      episode: movie.episode_id,
+      releaseYear: movie.release_date.slice(0, 4)
     }
   })
 }
 
 export const getCharacters = (id) => {
-  const url = `https://swapi.co/api/films/${id}`
-
-  return fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}, please retry`)
+  let url = `https://swapi.co/api/films/${id}`
+  return fetch(url).then(res => {
+    if(!res.ok) {
+      throw Error('Unable to fetch characters, click the movies tab and try again')
     }
-    return response.json()
-  })
+    return res.json()})
   .then(data => data.characters)
   .then(data => cleanCharacterData(data))
-  .then(chars => getCharacterHomeworldData(chars))
-  .then(chars => getCharacterSpeciesData(chars))
-  .then(chars => getCharacterFilmsData(chars))
+  .then(chars => getSpeciesDataForCharacter(chars))
+  .then(chars => getHomeWorldDataForCharacter(chars))
+  .then(chars => getRelatedFilmsForCharacter(chars))
+
 }
 
-const getCharacterHomeworldData = (chars) => {
-  const homeworldData = chars.map(char => {
-    return fetch(char.homeworld)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}, please retry`)
+const cleanCharacterData = (characterUrls) => {
+  let charactersData = characterUrls.map(charUrl => {
+    return fetch(charUrl).then(res => res.json()).then(data => {
+      return {
+        name:data.name,
+        homeworld: data.homeworld,
+        species: data.species,
+        films: data.films
       }
-      return response.json()
     })
-    .then(homeworld => {
+  })
+  return Promise.all(charactersData)
+}
+
+const getSpeciesDataForCharacter = (chars) => {
+  let speciesData = chars.map(char => {
+    if (char.species.length === 0) {
       return {
         ...char,
-        name: homeworld.name,
+        species: 'Unknown'
+      }
+    }
+    return fetch(char.species).then(res => res.json()).then(species => {
+      return {
+        ...char,
+        species: species.name
+        }
+      })
+    }
+  )
+  return Promise.all(speciesData)
+}
+
+const getHomeWorldDataForCharacter = (chars) => {
+  let homeworldData = chars.map(char => {
+    return fetch(char.homeworld).then(res => res.json()).then(homeworld => {
+      return {
+        ...char,
+        homeworld: homeworld.name,
         population: homeworld.population
       }
     })
@@ -60,66 +85,18 @@ const getCharacterHomeworldData = (chars) => {
   return Promise.all(homeworldData)
 }
 
-const getCharacterSpeciesData = (chars) => {
-  const speciciesData = chars.map(char => {
-    return fetch(char.species)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}, please retry`)
-      }
-      return response.json()
+const getRelatedFilmsForCharacter = (chars) => {
+  let relatedFilmsData = chars.map(char => {
+    let filmNames = char.films.map(film => {
+      return fetch(film).then(res => res.json()).then(film => film.title)
     })
-    .then(specie => {
+    return Promise.all(filmNames).then(namesOfFilms => {
       return {
         ...char,
-        name: specie.name
+        films: namesOfFilms
       }
-    })
+    }
+    )
   })
-  return Promise.all(speciciesData)
-}
-
-const getCharacterFilmsData = (chars) => {
- const filmsData = chars.map(char => {
-   const filmsName = char.films.map(film => {
-     return fetch(film)
-     .then(response => {
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}, please retry`)
-      }
-      return response.json()
-    })
-    .then(film => film.title)
-   })
-   return Promise.all(filmsName)
-   .then(namesOfFilms => {
-     return {
-       ...char,
-       films: namesOfFilms
-     }
-   })
- })
- return Promise.all(filmsData)
-}
-
-const cleanCharacterData = (characterUrls) => {
-  const characterData = characterUrls.map(charUrl => {
-    return fetch(charUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}, please retry`)
-      }
-      return response.json()
-    })
-    .then(data => {
-      return {
-        name: data.name,
-        homeworld: data.homeworld,
-        species: data.species,
-        films: data.films
-      }
-
-    })
-  })
-  return Promise.all(characterData)
+  return Promise.all(relatedFilmsData)
 }
