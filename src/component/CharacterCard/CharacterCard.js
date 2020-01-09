339eefca4a1566fa8b06f1a2498df91813d6f1c9
TheMindset <mulezig2014@outlook.fr>
2020-01-09 14:31:21 +0100

    Add CharacterCard

---
+++ src/component/CharacterCard/CharacterCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CharacterCard = ({ character }) => {
+  const { name, homeworld, population, species, films } = character
+
+  const allFilms = films.map(film => {
+    return <li> {film} </li>
+  })
+
+  return (
+    <div className="character-card">
+      <h2>{name}</h2>
+      <h3>Homeworld: {homeworld}</h3>
+      <h3>Population: {population} </h3>
+      <h3>Species: {species} </h3>
+      <ul>Films: {allFilms}</ul>
+    </div>
+  )
+}
+
+export default CharacterCard
+
+CharacterCard.propTypes = {
+  character: PropTypes.object.isRequired
+}
