549f5424be1c0937a818f2f160594b37cec24e77
TheMindset <mulezig2014@outlook.fr>
2020-01-10 23:40:03 +0100

    Add logic for display logic if exist

--- src/component/Container/Container.js
+++ src/component/Container/Container.js
@@ -4,12 +4,24 @@ import PropTypes from 'prop-types'
 import MovieCard from '../MovieCard/MovieCard'
 import CharacterCard from '../CharacterCard/CharacterCard'
 
-const Container = ({ cards, reachMovieCharacters }) => {
-  const allCards = cards.map(card => {
+const Container = ({ cards, reachMovieCharacters, toggleFavorite, type, favorites }) => {
+  console.log(favorites)
+    if (type === 'favorites' && favorites.length === 0) {
+      return (
+        <div className="empty-favorites">
+          <h3>Add some Favorites</h3>
+        </div>
+      )
+    }
+
+  let selectedCards = cards
+  if (type === 'characters')
+  {selectedCards = cards.slice(0, 10)}
+  const allCards = selectedCards.map(card => {
     if (card.title) {
       return <MovieCard key={card.episode} movie={card} reachMovieCharacters={reachMovieCharacters}/>
     }
-      return <CharacterCard key={card.name} character={card}/>
+      return <CharacterCard key={card.name} character={card} toggleFavorite={toggleFavorite} favorites={favorites} />
   })
 
   return (
