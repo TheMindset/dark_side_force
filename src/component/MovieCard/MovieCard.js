627aca9c5c2c38d78a67e55d60cceaae89bb7953
TheMindset <mulezig2014@outlook.fr>
2020-01-10 16:04:20 +0100

    Fix bug scroll displaying

--- src/component/MovieCard/MovieCard.js
+++ src/component/MovieCard/MovieCard.js
@@ -10,7 +10,7 @@ const MovieCard = ({ movie, reachMovieCharacters }) => {
       <h2> {title} </h2>
       <h3> Episode: {episode} </h3>
       <h3> Released in {releaseYear} </h3>
-      <Link onClick={reachMovieCharacters} to={`/movies/${id}`}>
+      <Link onClick={reachMovieCharacters} to={`movies/${id}`}>
         <button id={id}>Explore Characters</button>
       </Link>
     </div>
