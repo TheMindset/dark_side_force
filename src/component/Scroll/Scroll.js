501a518d8d6fdf0df85c23077bcee5306bee65ab
TheMindset <mulezig2014@outlook.fr>
2020-01-10 16:02:24 +0100

    Add scroll component

---
+++ src/component/Scroll/Scroll.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Scroll.scss'
+
+const Scroll = ({ selectedMovie }) => {
+  const { title, scrollText, episode } = selectedMovie
+  return (
+    <>
+      <div className="fade">
+          <section className="star-wars">
+            <div className="crawl">
+              <div className="title">
+                <p>Episode {episode}</p>
+                <h1>{title}</h1>
+              </div>
+                <p>{scrollText}</p>
+            </div>
+          </section>
+      </div>
+    </>
+  )
+}
+
+export default Scroll
+
+Scroll.propTypes = {
+  selectedMovie: PropTypes.object.isRequired
+}
