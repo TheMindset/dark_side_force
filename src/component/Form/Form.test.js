8ef8bd440c821cd73a98ddf0a5c8b1f7d239b616
TheMindset <mulezig2014@outlook.fr>
2020-01-12 21:00:04 +0100

    Add some test Component

--- src/component/Form/Form.test.js
+++ src/component/Form/Form.test.js
@@ -15,10 +15,10 @@ describe('Form', () => {
   })
   
   test('should update state when handleChange is called', () => {
-    const mockEvent = { target: { name: 'name', value: 'Hola' }, preventDefault: jest.fn() }
+    const mockNameEvent = { target: { name: 'name', value: 'Hola' }, preventDefault: jest.fn() }
 
     const expected = 'Hola'
-    wrapper.instance().handleChange(mockEvent)
+    wrapper.instance().handleChange(mockNameEvent)
     expect(wrapper.state('name')).toEqual(expected)
   })
 
@@ -32,9 +32,46 @@ describe('Form', () => {
     })
 
     wrapper.find('button').simulate('click', prevDefaultEvent)
-
     expect(mockGetFormData).toHaveBeenCalled()
   })
   
+  test('should return IsComplete false and an error if the quote is miising when Submit is clicked', () => {
+    const mockEvent = { target: { name: 'name', value: 'Terminator' } }
+
+    const prevDefaultEvent = { preventDefault: jest.fn() }
+
+    wrapper.instance().handleChange(mockEvent)
+    wrapper.find('button').simulate('click', prevDefaultEvent)
+
+    expect(wrapper.state('isComplete')).toEqual(false)
+    expect(wrapper.state('quoteErr')).toEqual(true)
+  })
   
+  test('should return IsComplete false and an error if the name is miising when Submit is clicked', () => {
+    const mockEvent = { target: { name: 'quote', value: 'I am the master of my destiny' } }
+
+    const prevDefaultEvent = { preventDefault: jest.fn() }
+
+    wrapper.instance().handleChange(mockEvent)
+    wrapper.find('button').simulate('click', prevDefaultEvent)
+
+    expect(wrapper.state('isComplete')).toEqual(false)
+    expect(wrapper.state('nameErr')).toEqual(true)
+  })
+
+  test('should run handleChange when the inputs change', () => {
+    const mockNameEvent = { target: { name: 'name', value: 'Viola Davis' } }
+    const mockQuoteEvent = { target: { name: 'quote', value: 'Think and grow' } }
+
+    wrapper.instance().handleChange = jest.fn()
+
+    wrapper.instance().forceUpdate()
+
+    wrapper.find('[name="name"]').simulate('change', mockNameEvent)
+    wrapper.find('[name="quote"]').simulate('change', mockQuoteEvent)
+
+    expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockNameEvent)
+    expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockQuoteEvent)
+  })
+
 })
